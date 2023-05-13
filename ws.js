e8ff3f030554df08d2bab917707d1351d991032f
Tosone <i@tosone.cn>
2023-05-13 09:35:10 +0800

    feat: first commit

---
+++ ws.js
@@ -0,0 +1,32 @@
+import ws from "k6/ws";
+
+export let options = {
+  stages: [
+    { duration: "1m", target: 3 },
+    { duration: "5m", target: 3 },
+  ],
+};
+
+export default function () {
+  const url = "wss://bgapi.bitget47.top/socket.io/?EIO=3&transport=websocket";
+  const response = ws.connect(url, {}, function (socket) {
+    socket.on("open", function open() {
+      console.log("connected");
+      socket.send('42["sub","kline_1_3_1min"]');
+    });
+
+    socket.on("message", function (message) {
+      console.log(`Received message: ${message}`);
+    });
+
+    socket.on("close", function () {
+      console.log("disconnected");
+    });
+  });
+
+  if (!response) {
+    console.log("WebSocket handshake failed");
+    return;
+  }
+}
+
